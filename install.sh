73beabc1c58819f108126aabb58ac8b95abbef4f
Daniel White <danrwhite99@yahoo.com>
2018-02-03 14:01:54 +1100

    Install update

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo apt-get install software-properties-common libboost-all-dev libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils -y
 
-sudo add-apt-repository ppa:bitcoin/bitcoin 
+sudo add-apt-repository ppa:bitcoin/bitcoin -y
 
 sudo apt-get update
 
